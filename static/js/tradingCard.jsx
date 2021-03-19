6a3d8258b4f12be41840fa62a32d229ef461793a
Sam <samantha.gerrity@gmail.com>
2021-03-19 00:56:21 +0000

    Psuedocoded for loop for render & added tc data
    Co-Authored-By: Kelsi Flatland <kelsi.flatland@gmail.com>

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,11 +1,67 @@
 "use strict";
+
 const tradingCardData = [
   {
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
   }
-]
+];
+
+// print name and skill
+
+// for card in tradingcarddata:
+//    print card[name], card[skill]
+
+// for (card of tradingCardData) {
+//   console.log(card['name'], card['skill'])
+// }
+
+
+
 
 function TradingCard(props) {
   return (
@@ -71,3 +127,17 @@ ReactDOM.render(
   ),
   document.querySelector('#shortstack-overflow')
 );
+
+// Pseudocode
+// define TradingCard component
+   // render one trading card
+
+// define TradingCardContainer component
+
+  // loop over each data point
+  // and make a TradingCard component
+  // return all those TradingCard's we made
+
+
+// mount just ONE thing onto the page:
+// one TradingCardContainer component
